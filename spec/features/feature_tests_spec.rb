1b35b087d4f3382d7ee978cd1aea125654bacff9
Colin McCulloch <mccolane@gmail.com>
2015-12-09 14:05:49 +0000

    Pt 20 second user story

--- spec/features/feature_tests_spec.rb
+++ spec/features/feature_tests_spec.rb
@@ -12,3 +12,16 @@ feature 'enter player names' do
     expect(page).to have_content 'Dave vs. Joan'
   end
 end
+
+# As Player 1,
+# So I can see how close I am to winning
+# I want to see Player 2's Hit Points
+feature 'display player 2 hitpoints' do
+  scenario 'display player 2 hit points' do
+  visit '/'
+  fill_in(:player1, with: 'Dave')
+  fill_in(:player2, with: 'Joan')
+  click_button 'Submit'
+  expect(page).to have_content 'Joan 77hp'
+  end
+end
